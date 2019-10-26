a98951f23cb698dab39236dcd1d2969d1c4a7976
Macbook <lmchvz@outlook.com>
2019-10-25 22:20:55 -0400

    adding files ERD, table schemata, queries

---
+++ EmployeeSQL/02_table_schemata.sql
@@ -0,0 +1,86 @@
+﻿DROP TABLE "Dept_Manager";
+DROP TABLE "Dept_Emp";
+Drop TABLE "Employees";
+DROP TABLE "Salaries";
+DROP TABLE "Titles";
+DROP TABLE "Departments"
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/7FEshF
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Employees" (
+    "emp_no" int   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(20)   NOT NULL,
+    "last_name" varchar(20)   NOT NULL,
+    "gender" char   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+    )
+);
+
+CREATE TABLE "Dept_Manager" (
+    "dept_no" varchar(20)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "Dept_Emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(20)   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "Titles" (
+    "emp_no" int   NOT NULL,
+    "title" varchar(30)   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "Departments" (
+    "dept_no" varchar(20)   NOT NULL,
+    "dept_name" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+SELECT * FROM "Departments"
+SELECT * FROM "Dept_Emp"
+SELECT * FROM "Dept_Manager"
+SELECT * FROM "Employees"
+SELECT * FROM "Salaries"
+SELECT * FROM "Titles"
+
+
---
+++ EmployeeSQL/03_ queries.sql
@@ -0,0 +1,70 @@
+SELECT * FROM "Departments"
+SELECT * FROM "Dept_Emp"
+SELECT * FROM "Dept_Manager"
+SELECT * FROM "Employees"
+SELECT * FROM "Salaries"
+SELECT * FROM "Titles"
+
+-- List the following details of each employee: employee number, last name, first name, gender, and salary.
+
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+FROM "Salaries" AS s
+INNER JOIN "Employees" AS e ON
+e.emp_no = s.emp_no;
+
+-- List employees who were hired in 1986.
+
+SELECT * FROM "Employees" 
+WHERE hire_date >= '1986-01-01' 
+AND hire_date < '1987-01-01'
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
+
+SELECT d.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name, m.from_date, m.to_date
+FROM "Departments" AS d
+INNER JOIN "Dept_Manager" AS m 
+ON m.dept_no = d.dept_no
+JOIN "Employees" AS e 
+ON e.emp_no = m.emp_no;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+SELECT  e.emp_no,e.last_name, e.first_name, d.dept_name
+FROM "Employees" AS e
+INNER JOIN "Dept_Emp" AS de
+ON e.emp_no = de.emp_no
+INNER JOIN "Departments" AS d
+On de.dept_no = d.dept_no;
+
+-- List all employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT * FROM "Employees"
+WHERE first_name LIKE 'Hercules'
+AND last_name LIKE 'B%';
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+SELECT  e.emp_no,e.last_name, e.first_name, d.dept_name
+FROM "Employees" AS e
+INNER JOIN "Dept_Emp" AS de
+ON e.emp_no = de.emp_no
+INNER JOIN "Departments" AS d
+On de.dept_no = d.dept_no
+WHERE dept_name like 'Sales';
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+SELECT  e.emp_no,e.last_name, e.first_name, d.dept_name
+FROM "Employees" AS e
+INNER JOIN "Dept_Emp" AS de
+ON e.emp_no = de.emp_no
+INNER JOIN "Departments" AS d
+On de.dept_no = d.dept_no
+WHERE dept_name like 'Sales'
+OR dept_name like 'Development';
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(*) AS frequency
+FROM "Employees"
+GROUP BY last_name
+ORDER BY frequency DESC;
